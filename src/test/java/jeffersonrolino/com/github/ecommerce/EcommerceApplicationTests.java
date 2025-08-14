fc20f4d34b3de204ab692697ea7a661eee2bd869
jeffersonrolino <jefferson.rolino@gmail.com>
2025-08-14 17:23:00 -0300

    initial spring boot project boilerplate

---
+++ src/test/java/jeffersonrolino/com/github/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package jeffersonrolino.com.github.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
