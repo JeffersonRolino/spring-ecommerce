fa1a4a271ec314a6715c62ea4f52b41243a2e2c5
jeffersonrolino <jefferson.rolino@gmail.com>
2025-08-26 07:46:31 -0300

    feat: fetching all categories

---
+++ src/main/java/jeffersonrolino/com/github/ecommerce/controller/CategoryController.java
@@ -0,0 +1,18 @@
+package jeffersonrolino.com.github.ecommerce.controller;
+
+import jeffersonrolino.com.github.ecommerce.model.Category;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class CategoryController {
+    private List<Category> categories = new ArrayList<>();
+
+    @GetMapping("/api/public/categories")
+    public List<Category> getAllCategories(){
+        return categories;
+    }
+}
