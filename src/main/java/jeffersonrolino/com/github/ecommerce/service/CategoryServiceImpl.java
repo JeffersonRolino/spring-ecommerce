1fd43d5975528c887f0d12dd301f5b4d6df36993
jeffersonrolino <jefferson.rolino@gmail.com>
2025-08-26 08:50:37 -0300

    feat: managing ID's

--- src/main/java/jeffersonrolino/com/github/ecommerce/service/CategoryServiceImpl.java
+++ src/main/java/jeffersonrolino/com/github/ecommerce/service/CategoryServiceImpl.java
@@ -17,6 +17,7 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public void createCategory(Category category) {
+        category.setId((long)categories.size() + 1);
         categories.add(category);
     }
 }
